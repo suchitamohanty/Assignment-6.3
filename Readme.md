c247c1f011a314244c5ab9d8ba15a983a1da67d0
Suchita Mohanty <mohanty.suchita@gmail.com>
2017-01-23 09:15:57 +0530

    A JavaScript program to display the sum upto 1000 using for and while loop

---
+++ Readme.md
@@ -0,0 +1,4 @@
+Session 5: JavaScript
+Assignment – Write a JavaScript program to display the sum up
+to 1000 using for and while loop and display the output in
+console
---
+++ js/app.js
@@ -0,0 +1,16 @@
+var i;
+var sum = 0;
+for (i = 0; i <= 1000; i++) {
+ sum = sum + i;
+}
+console.log(sum);
+
+
+var i = 0, sum = 0;
+while (i <= 1000) {
+	sum = sum + i;
+	i++;
+}
+console.log(sum);
+
+
